bf24d3032e87efdea39f45a518b69b794f7e1a2b
aahsnr-common <ahsan.05rahman@gmail.com>
2026-09-02 08:02:01 +0600

    feat: add math.ipynb notebook and refresh environment config and dependencies

--- src/sample.py
+++ src/sample.py
@@ -1,3 +1,11 @@
+from sage.all import *
+
+p = plot(exp(x), (x, -3, 3), color="blue", axes_labels=["$x$", "$e^x$"])
+
+p.show()
+
 import numpy as np
 
-arry1 = np.array([1, 2, 3])
+arry1 = np.array([1,2,3])
+
+arry1
